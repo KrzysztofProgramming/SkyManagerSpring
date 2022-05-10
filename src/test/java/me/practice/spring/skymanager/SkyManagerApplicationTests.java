186cc61bb89eeace982f48e21199dcff87ae3ee4
KrzysztofProgramming <krzychuk112@gmail.com>
2022-05-10 16:59:40 +0200

    corrections

--- src/test/java/me/practice/spring/skymanager/SkyManagerApplicationTests.java
+++ src/test/java/me/practice/spring/skymanager/SkyManagerApplicationTests.java
@@ -22,69 +22,69 @@ import java.util.TimeZone;
 
 @SpringBootTest
 class SkyManagerApplicationTests {
-
-	@Autowired
-	private CargoRepository cargoRepository;
-
-	@Autowired
-	private BaggageRepository baggageRepository;
-
-	@Autowired
-	private FlightRepository flightRepository;
-
-	@Autowired
-	private FlightSearcher flightSearcher;
-
+//
+//	@Autowired
+//	private CargoRepository cargoRepository;
+//
+//	@Autowired
+//	private BaggageRepository baggageRepository;
+//
+//	@Autowired
+//	private FlightRepository flightRepository;
+//
+//	@Autowired
+//	private FlightSearcher flightSearcher;
+//
 	@Test
 	void contextLoads() {
 	}
 
-	@Test
-	void createFlight(){
-		Flight flight = Flight.builder()
-				.flightId(1L)
-				.flightNumber(1232L)
-				.arrivalIATACode("KRK")
-				.departureIATACode("LND")
-				.departureDate(new Date()).build();
-		this.flightRepository.save(flight);
-	}
-
-	@Test
-	void createCargoAndBaggage(){
-		FlightBaggage baggage = FlightBaggage.builder()
-				.id(new LoadIdentifier(1L, 1L))
-				.pieces(5)
-				.weightKg(323.132)
-				.build();
-		FlightCargo cargo = FlightCargo.builder()
-				.id(new LoadIdentifier(1L, 1L))
-				.pieces(10)
-				.weightKg(132.3123)
-				.build();
-		this.baggageRepository.save(baggage);
-		this.cargoRepository.save(cargo);
-	}
-
-	@Test
-	@Transactional
-	void showFlights(){
-		System.out.println(this.flightRepository.findAll());
-	}
-
-
-	@Test
-	void testSearch(){
-		Calendar calendar = new GregorianCalendar();
-//		calendar.setTimeZone(TimeZone.getTimeZone("America/Mexico_City"));
-		calendar.set(Calendar.YEAR, 2014);
-		calendar.set(Calendar.MONTH, 0);
-		calendar.set(Calendar.DAY_OF_MONTH, 1);
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-		System.out.println(this.flightSearcher.statsByDateAndCode(calendar.getTime(), new Date(), "KRK"));
-		System.out.println(this.flightSearcher.statsByDateAndNumber(calendar.getTime(), new Date(), 1232L));
-	}
+//	@Test
+//	void createFlight(){
+//		Flight flight = Flight.builder()
+//				.flightId(1L)
+//				.flightNumber(1232L)
+//				.arrivalIATACode("KRK")
+//				.departureIATACode("LND")
+//				.departureDate(new Date()).build();
+//		this.flightRepository.save(flight);
+//	}
+//
+//	@Test
+//	void createCargoAndBaggage(){
+//		FlightBaggage baggage = FlightBaggage.builder()
+//				.id(new LoadIdentifier(1L, 1L))
+//				.pieces(5)
+//				.weightKg(323.132)
+//				.build();
+//		FlightCargo cargo = FlightCargo.builder()
+//				.id(new LoadIdentifier(1L, 1L))
+//				.pieces(10)
+//				.weightKg(132.3123)
+//				.build();
+//		this.baggageRepository.save(baggage);
+//		this.cargoRepository.save(cargo);
+//	}
+//
+//	@Test
+//	@Transactional
+//	void showFlights(){
+//		System.out.println(this.flightRepository.findAll());
+//	}
+//
+//
+//	@Test
+//	void testSearch(){
+//		Calendar calendar = new GregorianCalendar();
+////		calendar.setTimeZone(TimeZone.getTimeZone("America/Mexico_City"));
+//		calendar.set(Calendar.YEAR, 2014);
+//		calendar.set(Calendar.MONTH, 0);
+//		calendar.set(Calendar.DAY_OF_MONTH, 1);
+//		calendar.set(Calendar.HOUR_OF_DAY, 0);
+//		calendar.set(Calendar.MINUTE, 0);
+//		calendar.set(Calendar.SECOND, 0);
+//		calendar.set(Calendar.MILLISECOND, 0);
+//		System.out.println(this.flightSearcher.statsByDateAndCode(calendar.getTime(), new Date(), "KRK"));
+//		System.out.println(this.flightSearcher.statsByDateAndNumber(calendar.getTime(), new Date(), 1232L));
+//	}
 }
